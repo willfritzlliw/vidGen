5b4deb2feac6e4dbde23b05127f72583a49368ed
William Fritz <willfritzlliw@gmail.com>
2023-06-15 08:46:40 -0400

    Added Spanish title and tags

--- captions.py
+++ captions.py
@@ -2,10 +2,12 @@ from random import randint
 
 def get_tags(tag1: str):
     tags = ["Modivation","Chill out","Beats","Study","Work","Focus","Gaming","School","College","Reading","Ambient","Walking","Coding","Feel Good","Deep Focus","Brain Power","Brain Boost","Mood","Smooth"]
+    tags2 = ["Motivación","Relajarse","Latidos","Estudiar","Trabajar","Enfocar","Juego de azar","Escuela","Colega","Lectura",
+             "Ambiente","Caminando","Codificación","Sentirse bien","Enfoque profundo","Poder cerebral","Impulso cerebral","Ánimo","Liso"]
 
     randy = randint(0,(len(tags)-5))
 
-    return f"{tag1},Music,Compilation,{tags[randy]},{tags[randy+1]},{tags[randy+2]},{tags[randy+3]},{tags[randy+4]}"
+    return f"{tag1},Music,Compilation,{tags[randy]},{tags[randy+1]},{tags[randy+2]},{tags[randy+3]},{tags[randy+4]},{tags2[randy]},{tags2[randy+1]},{tags2[randy+2]},{tags2[randy+3]},{tags2[randy+4]}"
 
 def get_title(tag: str):
     tag = tag.capitalize()
@@ -19,6 +21,17 @@ def get_title(tag: str):
         "Music for Concentration 🧠",
         "Music for Creativity 💭",
         "Music for Productivity 🔥"
+    ]
+
+    titleMainsEs = [
+        "Música Para Juegos 🎮",
+        "Estudiar Musica 📚",
+        "Música de Codificación 💻",
+        "Música de Trabajo 🛠️",
+        "Música Para la Concentración 🧠",
+        "Música Para la Creatividad 💭",
+        "Música Para la Productividad 🔥"
+
     ]
 
     titleAddrs = [
@@ -47,7 +60,7 @@ def get_title(tag: str):
     while randy3 == randy1 or randy3 == randy2:
         randy3 = randint(0,(len(titleAddrs)-1))
 
-    title = titleMains[randTitle] + f" - {tag} - {titleAddrs[randy1]} - {titleAddrs[randy2]}"
+    title = titleMains[randTitle] + f" - {tag} - {titleAddrs[randy1]} - {titleAddrs[randy2]} | {titleMainsEs[randTitle]}"
 
     return title
 
@@ -57,11 +70,11 @@ def get_description(tag: str):
 
     descAdders = [
         "listen and enjoy while you study and work!",
-        ", thank you for listening!",
+        "- thank you for listening!",
         "Keep focused with this ambient music.",
         "to help you study, read, concentrate and stay motivated!",
         "thanks for tuning in, hope you enjoy!",
-        "to put you in in a good mode and headspace.",
+        "to put you in in a good mood and headspace.",
         "for deep focus,",
         "to boost your mood!",
         "to increase your concentration and productivity!",
@@ -71,7 +84,7 @@ def get_description(tag: str):
 
     randy = randint(0,(len(descAdders)-1))
 
-    desc = desc + f" {descAdders[randy]} Please like and subscribe for more!! \n"
+    desc = desc + f" {descAdders[randy]} Please Like and Subscribe!!\n"
 
     with open('desc_adder.txt') as f:
         for line in f.readlines():
@@ -79,4 +92,4 @@ def get_description(tag: str):
 
     return desc
 
-print(get_title('chill'))
+# Recopilacion de musica! Por favor, dale me gusta y suscríbete!
